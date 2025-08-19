5331cd6c6f568b968a9181f4fa81198ec89e1790
hurayrahSam <kimjeyeon03@icloud.com>
2025-08-19 22:23:34 +0900

    feat: intgrate middle ware and configmodule is global

--- apps/tourism/src/my-app.module.ts
+++ apps/tourism/src/my-app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { MyAppController } from './my-app.controller';
 import { MyAppService } from './my-app.service';
 import { PrismaModule } from 'libs/prisma';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule],
   controllers: [MyAppController],
   providers: [MyAppService],
 })
